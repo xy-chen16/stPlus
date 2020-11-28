52617e0d9b66b1cba0fa64061d2e23284f813d6c
xy-chen16 <xy-chen16@mails.tsinghua.edu.cn>
2020-11-28 22:57:55 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -10,32 +10,30 @@
 
 
 from setuptools import setup, find_packages
- 
+
 setup(
-  name = "stPlus",
-  version = "0.0.2",
-  keywords = ("pip", "stPlus"),
-  description = "stPlus: reference-based enhancement of spatial transcriptomics",
-  long_description = "",
-  license = "MIT Licence",
- 
-  url = "https://github.com/xy-chen16/stPlus",
-  author = "Xiaoyang Chen",
-  author_email = "xychen20@mails.tsinghua.edu.cn",
-  maintainer_email='ccq17@mails.tsinghua.edu.cn',
-
-
-  python_requires='>3.6.0',
-  classifiers=[
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.8',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX :: Linux',
-          'Topic :: Scientific/Engineering :: Bio-Informatics'
-     ],
-  install_requires=[
+    name="stPlus",
+    version="0.0.3",
+    keywords=("pip", "stPlus"),
+    description="stPlus: reference-based enhancement of spatial transcriptomics",
+    long_description="",
+    license="MIT Licence",
+    url="https://github.com/xy-chen16/stPlus",
+    author="Xiaoyang Chen",
+    author_email="xychen20@mails.tsinghua.edu.cn",
+    maintainer_email='ccq17@mails.tsinghua.edu.cn',
+    packages=find_packages(),
+    python_requires='>3.6.0',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+    install_requires=[
         'torch>=1.6.0',
         'torchvision>=0.7.0',
         'pandas>=1.1.0b',
@@ -44,3 +42,4 @@ setup(
         'scikit-learn>=0.23.2'
     ]
 )
+
